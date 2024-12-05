96c289c362b9f0d4733d5e3b84c4eed16743c000
brindersingh96 <163193057+brindersingh96@users.noreply.github.com>
2024-12-05 11:50:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    const portElement = document.getElementById('port');
+    const portNumber = 8081; // You can change this to any port you'd like
+    portElement.textContent = portNumber;
+};
